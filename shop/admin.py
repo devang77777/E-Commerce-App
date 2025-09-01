807f5049e26d2aeb4d9b2c8f9204eb15a6f0a862
Sugnesh90 <sugnesh@technobren.com>
2025-09-01 12:15:55 +0530

    Latest Code

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,33 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Product, ProductImage
+# from .models import Product,Contact,Order,OrderUpdate
 
-# Register your models here.
-from .models import Product,Contact,Order,OrderUpdate
-admin.site.register(Product) 
-admin.site.register(Contact) 
-admin.site.register(Order) 
-admin.site.register(OrderUpdate) 
+# # Register your models here.\
+# class ProductImageInline(admin.TabularInline):
+#     model = ProductImage
+#     extra = 1
+
+# class ProductAdmin(admin.ModelAdmin):
+#     inlines = [ProductImageInline]
+
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(Product) 
+# admin.site.register(Contact) 
+# admin.site.register(Order) 
+# admin.site.register(OrderUpdate) 
+
+from django.contrib import admin
+from .models import Product, ProductImage, Contact, Order, OrderUpdate
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage)
+admin.site.register(Contact)
+admin.site.register(Order)
+admin.site.register(OrderUpdate)
--- shop/models.py
+++ shop/models.py
@@ -14,7 +14,13 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="shop/images/")
 
+    def __str__(self):
+        return f"{self.product.product_name} Image"
+    
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
